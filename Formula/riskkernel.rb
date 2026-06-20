1cd031d97ee8a9c2bdd8e2ce106d1c8ac9425c0c
Adarsh Prashar <adarsh.prashar32@gmail.com>
2026-06-20 22:59:47 +0530

    riskkernel 0.9.0

--- Formula/riskkernel.rb
+++ Formula/riskkernel.rb
@@ -1,28 +1,28 @@
 class Riskkernel < Formula
   desc "Self-hosted reliability runtime for AI agents (budgets, approvals, crash-resume)"
   homepage "https://github.com/prashar32/riskkernel"
-  version "0.8.0"
+  version "0.9.0"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/prashar32/riskkernel/releases/download/v0.8.0/riskkernel_0.8.0_darwin_arm64.tar.gz"
-      sha256 "0dd6e860da9fda8236735eeb0dcf314777cff2a06d006f553876e13ba6873cd1"
+      url "https://github.com/prashar32/riskkernel/releases/download/v0.9.0/riskkernel_0.9.0_darwin_arm64.tar.gz"
+      sha256 "b2b48318aa82983592050cab3c74b7842a57d0cf3be150c267d4eec2b764bb75"
     end
     on_intel do
-      url "https://github.com/prashar32/riskkernel/releases/download/v0.8.0/riskkernel_0.8.0_darwin_amd64.tar.gz"
-      sha256 "bcf0ddf1bafe09ffc248289194314071916ce40fe17b96f7b906e25b3fca86f0"
+      url "https://github.com/prashar32/riskkernel/releases/download/v0.9.0/riskkernel_0.9.0_darwin_amd64.tar.gz"
+      sha256 "e65953536087f09eb8734eb2775d53e5fdceaa0fb8d5dd91a0d893f49eebe267"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/prashar32/riskkernel/releases/download/v0.8.0/riskkernel_0.8.0_linux_arm64.tar.gz"
-      sha256 "092c20944d04aaf199028779f0e09c58029a4e32ca2e96e854e9072b444229c6"
+      url "https://github.com/prashar32/riskkernel/releases/download/v0.9.0/riskkernel_0.9.0_linux_arm64.tar.gz"
+      sha256 "db5fc08561f1b8a35c0e41aac17137b0f397300bacdc3860e32aa771f191b77f"
     end
     on_intel do
-      url "https://github.com/prashar32/riskkernel/releases/download/v0.8.0/riskkernel_0.8.0_linux_amd64.tar.gz"
-      sha256 "25a1b9f25b14922a98c3f46d155aedb6167115eae233c50155226067f9a4b4d6"
+      url "https://github.com/prashar32/riskkernel/releases/download/v0.9.0/riskkernel_0.9.0_linux_amd64.tar.gz"
+      sha256 "2b3468618a4f8013d5b33a606256184fbf208a0e0d3e06282aeee0297af67eb2"
     end
   end
 
